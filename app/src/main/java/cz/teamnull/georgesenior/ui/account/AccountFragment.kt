4a193a414152e3eb80b025d89b0022e613907acf
Numbernineteen nineteen <developer.number19@gmail.com>
2021-10-16 21:51:01 +0200

    account screen

--- app/src/main/java/cz/teamnull/georgesenior/ui/account/AccountFragment.kt
+++ app/src/main/java/cz/teamnull/georgesenior/ui/account/AccountFragment.kt
@@ -1,12 +1,20 @@
 package cz.teamnull.georgesenior.ui.account
 
+import android.graphics.PorterDuff
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.RotateAnimation
+import androidx.core.content.ContextCompat
 import cz.teamnull.georgesenior.R
+import cz.teamnull.georgesenior.databinding.AccountFragmentBinding
 
 class AccountFragment : Fragment() {
 
@@ -15,18 +23,45 @@ class AccountFragment : Fragment() {
     }
 
     private lateinit var viewModel: AccountViewModel
+    private lateinit var binding: AccountFragmentBinding
+    private lateinit var micAnim: Animation
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.account_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = AccountFragmentBinding.bind(inflater.inflate(R.layout.account_fragment, container, false))
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.buttonListen.setOnClickListener {
+            viewModel.isListening = !viewModel.isListening
+            updateListeningButton()
+        }
+        micAnim = AnimationUtils.loadAnimation(requireContext(), R.anim.mic)
+    }
+
+    private fun updateListeningButton() = binding.apply {
+        if (viewModel.isListening) {
+            /*imageListening.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.white
+                ), PorterDuff.Mode.SRC_IN
+            )*/
+            imageListening.startAnimation(micAnim)
+            textViewListening.text = "Poslouchám"
+        } else {
+            /*imageListening.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.george_blue
+                ), PorterDuff.Mode.SRC_IN
+            )*/
+            imageListening.clearAnimation()
+            textViewListening.text = "Říct to"
+        }
+
     }
 
 }
--- app/src/main/java/cz/teamnull/georgesenior/ui/account/AccountViewModel.kt
+++ app/src/main/java/cz/teamnull/georgesenior/ui/account/AccountViewModel.kt
@@ -3,5 +3,13 @@ package cz.teamnull.georgesenior.ui.account
 import androidx.lifecycle.ViewModel
 
 class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var isListening = false
+
+
+
+
+    fun listen(){
+
+    }
+
 }
