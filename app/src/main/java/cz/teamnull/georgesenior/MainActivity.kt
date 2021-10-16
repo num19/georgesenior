38bab71703864345e7c1889fef23b297237d210a
Numbernineteen nineteen <developer.number19@gmail.com>
2021-10-16 14:21:55 +0200

    Initial Commit

---
+++ app/src/main/java/cz/teamnull/georgesenior/MainActivity.kt
@@ -0,0 +1,11 @@
+package cz.teamnull.georgesenior
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
